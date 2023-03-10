3d4131951461a5627d5081bc908fcc4b1aac1f7e
Fabio Luiz Vieira da Silva <fabinvieira@gmail.com>
2023-03-09 21:54:13 -0300

    creating routes, datafakes and content page

---
+++ src/app/data/datafake.ts
@@ -0,0 +1,14 @@
+export const dataFake = [
+  {
+    "id":"1",
+    "photoCover":"https://www.devmedia.com.br/arquivos/Artigos/29539/CSharp-Oo.jpg",
+    "title":"Saiu o preview do .Net 8",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit quam ac pretium aliquam. Nam commodo pellentesque massa, vel venenatis turpis. Proin nec pharetra risus, ut accumsan tellus. Pellentesque vulputate, ante vitae auctor aliquam, tortor arcu varius nisl, ac suscipit erat arcu in ex. Quisque et fringilla felis, sit amet pretium nisl. Duis ac ligula eu mi congue egestas. Vivamus rutrum quam nibh. Suspendisse facilisis urna vitae mi ullamcorper, quis sollicitudin leo consectetur. Morbi sed rutrum risus. Praesent placerat nulla odio, nec luctus tellus semper a. Fusce laoreet viverra tempus. Ut ac varius nibh, venenatis pretium sem. In sit amet pulvinar lorem. Nulla ex justo, blandit vel purus eget, suscipit posuere erat. Morbi vel ante dolor. Aliquam venenatis, ipsum in iaculis tristique, ante magna pretium diam, ultricies congue nibh odio sed lacus."
+  },
+  {
+    "id":"2",
+    "photoCover":"https://d33wubrfki0l68.cloudfront.net/f594274eb7ca4669d29d1a3662fea26d8f020965/669f6/images/category/development.jpg",
+    "title":"10 cursos de Python para você ficar craque",
+    "description":"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum blandit quam ac pretium aliquam. Nam commodo pellentesque massa, vel venenatis turpis. Proin nec pharetra risus, ut accumsan tellus. Pellentesque vulputate, ante vitae auctor aliquam, tortor arcu varius nisl, ac suscipit erat arcu in ex. Quisque et fringilla felis, sit amet pretium nisl. Duis ac ligula eu mi congue egestas. Vivamus rutrum quam nibh. Suspendisse facilisis urna vitae mi ullamcorper, quis sollicitudin leo consectetur. Morbi sed rutrum risus. Praesent placerat nulla odio, nec luctus tellus semper a. Fusce laoreet viverra tempus. Ut ac varius nibh, venenatis pretium sem. In sit amet pulvinar lorem. Nulla ex justo, blandit vel purus eget, suscipit posuere erat. Morbi vel ante dolor. Aliquam venenatis, ipsum in iaculis tristique, ante magna pretium diam, ultricies congue nibh odio sed lacus."
+  }
+]
